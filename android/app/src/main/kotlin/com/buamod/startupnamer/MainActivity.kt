0ab29f11698bb3f6784badc6e759ddf2f9c7dcfc
Ibrahim buamod <buamod@gmail.com>
2018-03-20 22:53:05 -0400

    Push project in one Commit Jiren style

---
+++ android/app/src/main/kotlin/com/buamod/startupnamer/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.buamod.startupnamer
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
